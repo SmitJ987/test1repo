35fa85d78df09822c3fb3b5b8e7f5822cd6d92b5
SmitJ987 <72332654+SmitJ987@users.noreply.github.com>
2020-10-04 12:14:26 +0530

    Add files via upload
    this file is having information explaining the introduction to git and Github
    thanks for exploring.

---
+++ week2.py
@@ -0,0 +1,66 @@
+#overview of GIT/github
+'''
+to understand overview of git/github
+we first need to understand version control
+version control allows us to keep track of changes to our document
+so it makes easy to recover older versions of our document if we make mistakes
+and it makes collaboration with others much easier
+see part of first video of week 2 from 00:58 min. to see picture form of
+with and without version control
+
+Git:
+free and open source software
+distributed version control system
+accessible anywhere in the world
+
+it is one of the most common version control systems available
+it can also version control images,documents etc.
+
+we can use Git from our command prompt
+
+but Github is one of the most popular web hosting services for the Git repository
+others include:
+GitLab
+BitBucket
+BeanStalk
+'''
+
+'''
+Short glossary of terms:
+    SSH: a method for secure remote login from one computer to another
+    Repository: The folders of our project that are setup for version control
+    Fork: it is a copy of a repository
+    Pull Request: The process we use to request that someone reviews and approve our changes before 
+                  they become final
+    Working Directory: a directory on our file system, including it's files and sub-directories,
+                        that is associated with a git repository.
+'''
+
+'''
+Few basic git Commands that we will always use:
+    "git init": when starting out with a new repository, we only need to create it once 
+                either locally and then push to github
+                or by cloning an existing repository 
+                by using the command "git init"
+                
+    "git add": git add moves changes from the working directory to the staging area
+    
+    "git status": allows us to see the state of our working directory and the staged snapshot
+                    of our changes
+    
+    "git commit": takes our staged snapshot of changes and commits them to the project
+    
+    "git reset": undoes changes  that we made to the files in our working directory
+    
+    "git log": enables us to browse previous changes to a project
+    
+    "git branch": it let's us create isolated environment within our repository to make changes
+    
+    "git checkout": it let's us to see and change existing branches
+    
+    "git merge": let's us put everything back together again  
+'''
+'''
+ go to https://try.github.io/ and download cheat sheets and practice the tutorials
+ 
+'''
